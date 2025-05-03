e651fdcd917cc508be05fea1ac943123cc9de6e6
Evan Pratama <pratamaevan2005@gmail.com>
2025-05-03 15:16:15 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/com/evanpratama0137/ciciluang/MainActivity.kt
+++ app/src/main/java/com/evanpratama0137/ciciluang/MainActivity.kt
@@ -1,28 +1,11 @@
 package com.evanpratama0137.ciciluang
 
-import android.accessibilityservice.AccessibilityService.ScreenshotResult
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import com.evanpratama0137.ciciluang.ui.screen.MainScreen
 import com.evanpratama0137.ciciluang.ui.theme.CicilUangTheme
-import com.evanpratama0137.ciciluang.ui.theme.WarnaContainer
-import com.evanpratama0137.ciciluang.ui.theme.WarnaText
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,39 +19,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = WarnaContainer,
-                    titleContentColor = WarnaText,
-                )
-            )
-        }
-    ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    CicilUangTheme {
-        MainScreen()
-    }
-}
--- app/src/main/java/com/evanpratama0137/ciciluang/ui/theme/Color.kt
+++ app/src/main/java/com/evanpratama0137/ciciluang/ui/theme/Color.kt
@@ -10,5 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val WarnaContainer = Color(0xFF9336B4)
-val WarnaText = Color(0xFFFFE79B)
+val WarnaContainer = Color(0xFF0066AE)
+val WarnaText = Color(0xFFffb300)
