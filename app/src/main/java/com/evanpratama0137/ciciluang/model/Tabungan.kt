4d0b280690f3967fd2e6ef75184731f14d408d0f
Evan Pratama <pratamaevan2005@gmail.com>
2025-05-03 22:57:59 +0700

    Menyiapkan data di ViewModel

---
+++ app/src/main/java/com/evanpratama0137/ciciluang/model/Tabungan.kt
@@ -0,0 +1,10 @@
+package com.evanpratama0137.ciciluang.model
+
+data class Tabungan(
+    val id: Long,
+    val nama: String,
+    val jumlah: Double,
+    val deskripsi: String,
+    val tanggal: String
+)
+
---
+++ app/src/main/java/com/evanpratama0137/ciciluang/ui/screen/MainViewModel.kt
@@ -0,0 +1,39 @@
+package com.evanpratama0137.ciciluang.ui.screen
+
+import androidx.lifecycle.ViewModel
+import com.evanpratama0137.ciciluang.model.Tabungan
+
+class MainViewModel : ViewModel() {
+
+    val data = listOf(
+        Tabungan(
+            1,
+            "Tabungan Laptop",
+            10_000_000.0,
+            "Tabungan ini diperuntukkan untuk membeli laptop",
+            "2025-06-20"
+        ),
+        Tabungan(
+            2,
+            "Tabungan HP",
+            3_500_000.0,
+            "Tabungan ini diperuntukkan untuk membeli HP",
+            "2025-05-25"
+        ),
+        Tabungan(
+            3,
+            "Dana Darurat",
+            4_000_000.0,
+            "Uang untuk keperluan yang darurat / segera",
+            "2025-06-15"
+        ),
+        Tabungan(
+            4,
+            "Tabungan Liburan",
+            5_000_000.0,
+            "Tabungan untuk nanti liburan",
+            "2025-05-10"
+
+        )
+    )
+}
